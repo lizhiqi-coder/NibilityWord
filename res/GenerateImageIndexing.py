a3106256b916b1d26491dc513c4ee96a9f7e53cf
lizhiqi <chinalizhiqi@gmail.com>
2017-02-20 20:05:05 +0800

    架构

---
+++ res/GenerateImageIndexing.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+
+def getImageList():
+    return [("file_name,file_path"), (), ()]
+
+
+def buildIndexing(index_list, namespace):
+    return
+
+
+if __name__ == '__main__':
+    image_paths = getImageList()
---
+++ src/SearchBanner.py
@@ -0,0 +1,51 @@
+# coding:utf-8
+import sys
+
+try:
+    from PySide.QtCore import *
+    from PySide.QtGui import *
+except ImportError:
+    from PyQt4.QtCore import *
+    from PyQt4.QtGui import *
+
+
+class SearchBanner(QMainWindow):
+    def __init__(self):
+        super(SearchBanner, self).__init__()
+        self.initTransform()
+        self.initTitle()
+        self.initInputBar()
+        self.initShowBar()
+
+    def initTransform(self):
+        self.setGeometry(0, 0, 420, 70)
+        self.center(self)
+        self.moveByCenter(100,100)
+
+    def initTitle(self):
+        self.setWindowTitle('NiubilitiyWord')
+        pass
+
+    def initInputBar(self):
+        pass
+
+    def initShowBar(self):
+        pass
+
+    def center(self, widget):
+        rect = widget.frameGeometry()
+        self.center_point = QDesktopWidget().availableGeometry().center()
+
+        rect.moveCenter(self.center_point)
+        widget.move(rect.topLeft())
+
+    def moveByCenter(self, x, y):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainBanner = SearchBanner()
+    mainBanner.show()
+
+    sys.exit(app.exec_())
