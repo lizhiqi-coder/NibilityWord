e9345caececea6a5ae61341994d0e98f204087ca
lzq <chinalizhiqi@gmail.com>
2017-03-07 00:31:28 +0800

    完成金山词霸 详细词条Model

--- src/interface/IPowerWord.py
+++ src/interface/IPowerWord.py
@@ -1,2 +1,33 @@
-#coding:utf-8
+# coding:utf-8
 """金山词霸"""
+import httplib
+
+# sys.path.append('..')
+from src.model.DetailModel import *
+
+myUrl = '/api/dictionary.php'
+secretKey = 'C75843D78A50DD1378BADC7A8BD3995D'
+
+
+def translate(question, type='json'):
+    httpClient = None
+    global myUrl
+    myUrl += '?w=' + question \
+             + '&type=' + type \
+             + '&key=' + secretKey
+    try:
+        httpClient = httplib.HTTPConnection('dict-co.iciba.com')
+        httpClient.request('GET', myUrl)
+        response = httpClient.getresponse()
+        print response.read()
+        return DetailModel()
+    except Exception, e:
+        print e
+    finally:
+        if httpClient:
+            httpClient.close()
+    return None
+
+
+if __name__ == '__main__':
+    translate('girl', type='json')
---
+++ src/model/DetailModel.py
@@ -0,0 +1,34 @@
+# coding:utf-8
+
+
+class exchange:
+    """单词变换形态"""
+
+    def __init__(self,
+                 word_pl='',
+                 word_third='',
+                 word_past='',
+                 word_done='',
+                 word_ing='',
+                 word_er='',
+                 word_est=''):
+        self.word_pl = word_pl
+        self.word_third = word_third
+        self.word_past = word_past
+        self.word_done = word_done
+        self.word_ing = word_ing
+        self.word_er = word_er
+        self.word_est = word_est
+
+
+class symbol:
+    def __init__(self, ph_en, ph_en_mp3, ph_am, ph_am_mp3, means):
+        self.ph = {ph_en: ph_en_mp3, ph_am: ph_am_mp3}
+        self.means = means  # list
+
+
+class DetailModel:
+    def __init__(self, word_name, exchange, symbols):
+        self.word_name = word_name
+        self.exchange = exchange
+        self.symbols = symbols  # list
