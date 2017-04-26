f15cf3859f92c98b9de29d48ba4365ea68f8c64b
lizhiqi <chinalizhiqi@gmail.com>
2017-04-26 20:35:11 +0800

    add 系统托盘

--- main.py
+++ main.py
@@ -11,11 +11,18 @@ except ImportError:
     from PyQt4.QtCore import *
     from PyQt4.QtGui import *
 
+from res import R
+
 
 def main():
     app = QApplication(sys.argv)
     mainBanner = SearchBanner()
+    trayIcon = QSystemTrayIcon()
+    trayIcon.setIcon(QIcon(R.png.dict))
+
     mainBanner.show()
+    trayIcon.show()
+    trayIcon.activated.connect(mainBanner.show)
     sys.exit(app.exec_())
     pass
 
