ec2463955e8905a2ba640725bb3ef346ea624053
lzq <chinalizhiqi@gmail.com>
2017-04-28 22:57:16 +0800

    优化下setting界面

--- src/SettingPage.py
+++ src/SettingPage.py
@@ -17,6 +17,8 @@ from utils import NBUtils
 关于我们
 检查更新
 """
+
+
 class SettingPage(QWidget):
     SCREEN_WIDTH = 700
     SCREEN_HEIGHT = 430
@@ -24,6 +26,7 @@ class SettingPage(QWidget):
     def __init__(self):
         super(SettingPage, self).__init__()
         self._initUI()
+        self.initContent()
 
     def _initUI(self):
         NBUtils.bindStyleSheet(self, R.qss.setting_page_style)
@@ -40,27 +43,42 @@ class SettingPage(QWidget):
         self.layout().setContentsMargins(0, 0, 0, 0)
         self.layout().setSpacing(0)
 
-        calalogue_frame = QFrame()
-        calalogue_frame.setFixedWidth(self.SCREEN_WIDTH / 6)
-        calalogue_frame.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Expanding)
-        content_frame = QFrame()
-        content_frame.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        catalogue_frame = QFrame()
+        # catalogue_frame.setFixedWidth(self.SCREEN_WIDTH / 6+30)
+        catalogue_frame.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Expanding)
+        self.content_frame = QFrame()
+        self.content_frame.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        self.layout().addWidget(calalogue_frame)
-        self.layout().addWidget(content_frame)
+        self.layout().addWidget(catalogue_frame)
+        self.layout().addWidget(self.content_frame)
 
         left_layout = QVBoxLayout()
         right_layout = QVBoxLayout()
-        calalogue_frame.setLayout(left_layout)
-        content_frame.setLayout(right_layout)
+        catalogue_frame.setLayout(left_layout)
+        self.content_frame.setLayout(right_layout)
 
-        calalogue_frame.layout().setContentsMargins(20, 20, 20, 20)
-        calalogue_frame.layout().setSpacing(10)
-        calalogue_frame.layout().setAlignment(Qt.AlignTop)
-        calalogue_frame.layout().addWidget(QPushButton('button'))
-        calalogue_frame.layout().addWidget(QPushButton('button'))
-        calalogue_frame.layout().addWidget(QPushButton('button'))
-        calalogue_frame.layout().addWidget(QPushButton('button'))
+        catalogue_frame.layout().setContentsMargins(20, 20, 20, 20)
+        catalogue_frame.layout().setSpacing(10)
+        catalogue_frame.layout().setAlignment(Qt.AlignTop)
+
+        self.btn_my_dict = CatalogButton(R.string.my_dict)
+        self.btn_shortcut = CatalogButton(R.string.shortcut)
+        self.btn_about_me = CatalogButton(R.string.about_me)
+        self.btn_check_update = CatalogButton(R.string.check_update)
+
+        self.btn_my_dict.toggled.connect(lambda: self.onBtnChecked(self.btn_my_dict, 1))
+        self.btn_shortcut.toggled.connect(lambda: self.onBtnChecked(self.btn_shortcut, 2))
+        self.btn_about_me.toggled.connect(lambda: self.onBtnChecked(self.btn_about_me, 3))
+        self.btn_check_update.toggled.connect(lambda: self.onBtnChecked(self.btn_check_update, 4))
+
+        catalogue_frame.layout().addWidget(self.btn_my_dict)
+        catalogue_frame.layout().addWidget(self.btn_shortcut)
+        catalogue_frame.layout().addWidget(self.btn_about_me)
+        catalogue_frame.layout().addWidget(self.btn_check_update)
+
+    def initContent(self):
+        self.pannel_about_me = QLabel("about me")
+        self.content_frame.layout().addWidget(self.pannel_about_me)
 
     def moveByCenter(self, x, y):
         rect = self.frameGeometry()
@@ -68,9 +86,29 @@ class SettingPage(QWidget):
         rect.moveCenter(self.center_point + QPoint(x, y))
         self.move(rect.topLeft())
 
+    def onBtnChecked(self, btn, btn_id):
+        if btn.isChecked():
+
+            if btn_id == 1:
+                print 1
+            elif btn_id == 2:
+                print 2
+            elif btn_id == 3:
+                self.pannel_about_me.show()
+            elif btn_id == 4:
+                print 4
+
+
+class CatalogButton(QRadioButton):
+    def __init__(self, text):
+        super(CatalogButton, self).__init__(text)
+        self.setObjectName('CatalogButton')
+        self.setFocusPolicy(Qt.NoFocus)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     setting = SettingPage()
     setting.show()
+    setting.moveByCenter(0, 0)
     sys.exit(app.exec_())
