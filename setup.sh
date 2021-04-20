4d458be4d95686e8a8c092124b5a84e0af9165f9
lizhiqi-ubuntu <lizhiqi.cpp@bytedance.com>
2021-04-20 17:34:40 +0800

    linux setup desktop shortcut

--- setup.sh
+++ setup.sh
@@ -7,3 +7,23 @@ pip install httplib2
 pip install pygame
 pip install evdev
 pip install wget
+
+WORKSPACE=$(cd `dirname $0`; pwd)
+
+target_desktop="/usr/share/applications/niubility-word.desktop"
+echo $target_desktop
+
+if [ -f $target_desktop ]; then
+   sudo rm $target_desktop
+fi
+sudo touch $target_desktop
+sudo chmod 777 $target_desktop
+
+sudo cat $WORKSPACE/res/nbword.desktop > $target_desktop
+sudo echo "Exec=python2.7 ${WORKSPACE}/main.py" >> $target_desktop
+sudo echo "Icon=${WORKSPACE}/res/app_icon.png" >> $target_desktop
+
+#https://blog.csdn.net/u014025444/article/details/94024282
+echo "--------------set desktop shortcut succeed !------------------------------"
+sudo cat $target_desktop
+echo "-------------------------------------------------------------------------"
